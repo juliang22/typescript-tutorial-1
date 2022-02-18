850a8c3aca22f8336351d3f8253cfd2423d33c7e
Julian Grunauer <Julian.A.Grunauer.21@Dartmouth.edu>
2022-02-18 11:44:42 -0800

    finished tutorial. typescript is kewl

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import List from './components/List'
+import AddToList from './components/AddToList';
+
+
+export interface IPeeps {
+  peeps: {
+    name: string;
+    age: number;
+    url: string;
+    note?: string
+  }[]
+}
 
 function App() {
+
+  const [peeps, setPeeps] = useState<IPeeps["peeps"]>([])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>People invited to my party</h1>
+      {<List peeps={peeps} />}
+      {<AddToList peeps={peeps} setPeeps={setPeeps} />}
     </div>
   );
 }
